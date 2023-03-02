bfd86de53fcb8416bed1c2f9ceea609636ecc4f1
matt maddex <mmaddex@render.com>
2023-03-02 16:25:17 -0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
         <p>{!data ? "Loading..." : data}</p>        
         <p>good bye from 2022</p>
         <p>hello 2023!!</p>
-        <p>hello PE</p>
+        <p>bye PE</p>
       </header>
     </div>
   );
