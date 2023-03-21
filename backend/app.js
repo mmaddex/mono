de85428c1d19ce182f0d9b693082dcbf2fb862eb
matt maddex <mmaddex@render.com>
2023-03-21 16:43:37 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,4 +7,10 @@ app.get('/hello', (req, res) => {
 	res.json('Hello World!, from Jabba the script!!');
 });
 
+app.get('/.well-known/test.json', (req, res) => {
+	res.set('Content-Length', '666');
+	res.json({"oh": "really"});
+});
+
+
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
