157e1222c7683532bcd03d53a1958963c664d53b
matt maddex <mmaddex@render.com>
2023-03-21 16:55:50 -0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,8 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/.well-known/test.json', (req, res) => {
-	res.set('Content-Length', '666');
-	res.set('x-test-Length', '666');
-	res.set('page-size', 20);
-	res.set('x-something', 'page-size');
+	res.set('content-length', '666');
+	res.set('transfer-encoding', 'deflate');
 	res.json({"oh": "really"});
 });
 
